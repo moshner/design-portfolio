ba415b4e659ed449edc2075706053bca231e2c32
Nick Mosher <nick@mosher.me>
2021-10-31 16:18:31 -0500

    refactor: strict rules applied

--- src/App.tsx
+++ src/App.tsx
@@ -1,48 +1,59 @@
 import React from 'react';
 import ProjectOverviewCard from './components/project-overview-card';
+// eslint-disable-next-line import/no-unassigned-import
 import './App.css';
 import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  Link
-} from "react-router-dom";
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link,
+} from 'react-router-dom';
+import type { VFC } from 'react';
 
-function App() {
-  return (
+const App: VFC = () => (
     <Router>
-      <header><img src="/monogram.png" alt="NM" /><Link to="/">Home</Link> | <Link to="/project1">Foo</Link> | <a href="https://nick.mosher.me">About Nick</a></header>
-      <Switch>
-        <Route path="/" exact>
-          <ProjectOverviewCard 
-            name="Project Name" 
-            link="//google.com"
-            linkText="google"
-            imgAlt="A cute kitten"
-            imgSrc="//placekitten.com/800/601" 
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ipsa perspiciatis quod in facilis aut, voluptate laudantium aperiam consectetur saepe, corrupti, iure commodi id dignissimos ullam eaque alias illum rerum." 
-          />
-          <ProjectOverviewCard 
-            name="Project Name" 
-            link="//google.com"
-            linkText="google"
-            imgAlt="A cute kitten"
-            imgSrc="//placekitten.com/800/601" 
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ipsa perspiciatis quod in facilis aut, voluptate laudantium aperiam consectetur saepe, corrupti, iure commodi id dignissimos ullam eaque alias illum rerum." 
-          />
-          <ProjectOverviewCard 
-            name="Project Name" 
-            link="//google.com"
-            linkText="google"
-            imgAlt="A cute kitten"
-            imgSrc="//placekitten.com/800/601" 
-            text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ipsa perspiciatis quod in facilis aut, voluptate laudantium aperiam consectetur saepe, corrupti, iure commodi id dignissimos ullam eaque alias illum rerum." 
-          />
-        </Route>
-      </Switch>
-      <footer></footer>
+        <header>
+            <img src="/monogram.png" alt="NM" />
+            <Link to="/">Home</Link>
+            {' '}
+            |
+            {' '}
+            <Link to="/project1">Foo</Link>
+            {' '}
+            |
+            {' '}
+            <a href="https://nick.mosher.me">About Nick</a>
+        </header>
+        <Switch>
+            <Route path="/" exact>
+                <ProjectOverviewCard
+                    name="Project Name"
+                    link="//google.com"
+                    linkText="google"
+                    imgAlt="A cute kitten"
+                    imgSrc="//placekitten.com/800/601"
+                    text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ipsa perspiciatis quod in facilis aut, voluptate laudantium aperiam consectetur saepe, corrupti, iure commodi id dignissimos ullam eaque alias illum rerum."
+                />
+                <ProjectOverviewCard
+                    name="Project Name"
+                    link="//google.com"
+                    linkText="google"
+                    imgAlt="A cute kitten"
+                    imgSrc="//placekitten.com/800/601"
+                    text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ipsa perspiciatis quod in facilis aut, voluptate laudantium aperiam consectetur saepe, corrupti, iure commodi id dignissimos ullam eaque alias illum rerum."
+                />
+                <ProjectOverviewCard
+                    name="Project Name"
+                    link="//google.com"
+                    linkText="google"
+                    imgAlt="A cute kitten"
+                    imgSrc="//placekitten.com/800/601"
+                    text="Lorem ipsum dolor sit amet consectetur adipisicing elit. Omnis ipsa perspiciatis quod in facilis aut, voluptate laudantium aperiam consectetur saepe, corrupti, iure commodi id dignissimos ullam eaque alias illum rerum."
+                />
+            </Route>
+        </Switch>
+        <footer />
     </Router>
-  );
-}
+);
 
 export default App;
--- src/components/project-overview-card.tsx
+++ src/components/project-overview-card.tsx
@@ -1,23 +1,28 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
+import type { VFC } from 'react';
 
-interface ComponenProps {
-    imgAlt?: string;
-    imgSrc?: string;
+interface ComponentProps {
+    imgAlt: string;
+    imgSrc: string;
     link: string;
-    linkText?: string;
-    name?: string;
-    text?: string;
+    linkText: string;
+    name: string;
+    text: string;
 }
 
-function ProjectOverviewCard({name, imgSrc, imgAlt, link, linkText, text}: ComponenProps) {
+const ProjectOverviewCard: VFC<ComponentProps> = (props: ComponentProps) => {
+    const {
+        name, imgSrc, imgAlt, link, linkText, text,
+    } = props;
+
     return (
         <section className="project-overview">
-        <h1 className="project-overview__name">{name}</h1>
-        <div className="project-overview__image-box"><img className="image-box__img" src={imgSrc} alt={imgAlt} /></div>
-        <p className="project-overview__text">{text}</p>
-        <Link className="project-overview__button" to={link}>{linkText}</Link>
+            <h1 className="project-overview__name">{name}</h1>
+            <div className="project-overview__image-box"><img className="image-box__img" src={imgSrc} alt={imgAlt} /></div>
+            <p className="project-overview__text">{text}</p>
+            <Link className="project-overview__button" to={link}>{linkText}</Link>
         </section>
-    )
-}
+    );
+};
 
-export default ProjectOverviewCard
+export default ProjectOverviewCard;
