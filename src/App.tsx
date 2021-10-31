5303c8d54084d9bf248e43c3ecdc3fd4e3367e3c
Nick Mosher <nick@mosher.me>
2021-10-31 15:12:45 -0500

    feat: initial app creation

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,31 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <header>Menu <Link to="/">Home</Link> | <Link to="/project1">Foo</Link> | <a href="https://nick.mosher.me">About Nick</a></header>
+      <Switch>
+        <Route path="/" exact>
+          <section><div>Image box</div><Link to="/project1">Foo</Link></section>
+          <section>Item 2</section>
+          <section>Item 3</section>
+        </Route>
+        <Route path="/project1">
+          <section><div>Image boxes</div></section>
+          <section>Item 2</section>
+          <section>Item 3</section>
+        </Route>
+      </Switch>
+      <footer></footer>
+    </Router>
   );
 }
 
